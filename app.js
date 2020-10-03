52c75732f87566107e461e4e064aad4ae7bb4841
nigg <thuse91@live.se>
2020-10-03 21:15:13 +0200

    added a few functions

--- app.js
+++ app.js
@@ -9,4 +9,58 @@ function add(){
        document.querySelector(".output").innerHTML = previousInput + parseInt(input.value)
        previousInput += parseInt(input.value);
     }
+    document.querySelector(".input").value = "";
+}
+
+function sub(){     
+    if(previousInput === null){
+        document.querySelector(".output").innerHTML -= input.value
+        previousInput = parseInt(input.value);
+    } else {
+       document.querySelector(".output").innerHTML = previousInput - parseInt(input.value)
+       previousInput -= parseInt(input.value);
+    }
+    document.querySelector(".input").value = "";
+}
+
+function mult(){     
+    if(previousInput === null){
+        document.querySelector(".output").innerHTML *= input.value
+        previousInput = parseInt(input.value);
+    } else {
+       document.querySelector(".output").innerHTML = previousInput * parseInt(input.value)
+       previousInput *= parseInt(input.value);
+    }
+    document.querySelector(".input").value = "";
+}
+
+function div(){     
+    if(previousInput === null){
+        document.querySelector(".output").innerHTML /= input.value
+        previousInput = parseInt(input.value);
+    } else {
+       document.querySelector(".output").innerHTML = previousInput / parseInt(input.value)
+       previousInput /= parseInt(input.value);
+    }
+    document.querySelector(".input").value = "";
+}
+
+function mod(){     
+    if(previousInput === null){
+        document.querySelector(".output").innerHTML %= input.value
+        previousInput = parseInt(input.value);
+    } else {
+       document.querySelector(".output").innerHTML = previousInput % parseInt(input.value)
+       previousInput %= parseInt(input.value);
+    }
+    document.querySelector(".input").value = "";
+}
+
+
+
+function reset(){
+    previousInput = null;
+    document.querySelector(".input").value = "";
+    document.querySelector(".output").innerHTML = "";
+    console.log(input)
 }
